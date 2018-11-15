702fe0c14eec6fa0871235d74d21416573ac329e
Shaniesha <shaniesha.john.94@gmail.com>
2018-11-15 15:27:57 -0500

    Initial

---
+++ app/src/main/java/com/android/champlaincollege1131288/addmemberpage/MainActivity.java
@@ -0,0 +1,13 @@
+package com.android.champlaincollege1131288.addmemberpage;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
